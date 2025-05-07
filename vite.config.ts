227ddbb92d46501647710cfcc2d8cb0778c7dfe5
phyrnna05 <phyrnna05@gmail.com>
2025-05-08 02:44:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss(),]
+  plugins: [react(),  tailwindcss(),],
+  server: {
+    host: true,
+    port: 8080,
+    allowedHosts: true, // ✅ allows any host
+  }
+  ,
 })
